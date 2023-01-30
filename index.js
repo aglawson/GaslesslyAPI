7f67b7e9e3f90260b3a548325817dc65975f4b68
Adrian Lawson <adrianglawson@gmail.com>
2023-01-30 11:17:33 -0800

    add documentation

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express'
 export const router = express.Router()
-const app = express()
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
 dotenv.config();
@@ -23,11 +22,24 @@ import { GetOwnedContracts } from './functions/GetOwnedContracts.js'
 
 router.use(bodyParser.json())
 
+/**
+ * Used to make sure only requests sent with api keys are allowed to access 
+ * protected endpoints. 
+ * 
+ * @returns bool
+ */
 function auth(req) {
     if(!process.env.API_KEYS.split(',').includes(req.query.api_key)) return false;
     return true;
 }
 
+/**
+ * Returns the amount of NFTs owned by the wallet for the given contract address
+ * @param req has following members
+ * wallet - wallet address whose balance is being checked
+ * contract - contract address of NFT
+ * network - 'goerli' or 'mainnet'
+ */
 router.get('/nft_balance', async (req, res) => {
     try{
         const result = await NFTBalance(req);
@@ -38,6 +50,13 @@ router.get('/nft_balance', async (req, res) => {
     }
 });
 
+/**
+ * Returns the amount of ERC20 tokens owned by the wallet for the given contract address
+ * @param req has following members
+ * wallet - wallet address whose balance is being checked
+ * contract - contract address of ERC20 token
+ * network - 'goerli' or 'mainnet'
+ */
 router.get('/token_balance', async (req, res) => { 
     try {
         const result = await TokenBalance(req);
@@ -49,6 +68,12 @@ router.get('/token_balance', async (req, res) => {
 
 });
 
+/**
+ * Returns the amount of ETH owned by the wallet
+ * @param req has following members
+ * wallet - wallet address whose balance is being checked
+ * network - 'goerli' or 'mainnet'
+ */
 router.get('/eth_balance', async (req, res) => {
     try{
         const result = await ETHBalance(req);
@@ -59,6 +84,16 @@ router.get('/eth_balance', async (req, res) => {
     }
 })
 
+/**
+ * @param req has following members
+ * wallet - wallet address on behalf of whom the tx is being sent
+ * contract - address of recipient of tx
+ * network - 'goerli' or 'mainnet' //currently only goerli supported
+ * signature - wallet's signature of the tx data
+ * reqStruct - struct containing tx data {from: address, to: address, value: uint, gas: uint, nonce: uint, data: (hash of tx data)}
+ * 
+ * @returns tx hash of resulting tx
+ */
 router.get('/relay', async (req, res) => {
     !auth(req) ? res.status(401).send('Access Denied') : console.log('authorized');
 
@@ -71,6 +106,13 @@ router.get('/relay', async (req, res) => {
     }
 })
 
+/**
+ * @param req has following members
+ * network - 'goerli'
+ * func - name of function with parameter types, ex. functionName(uint256,address,string)
+ * 
+ * @returns hash of tx data to be used in relay function
+ */
 router.get('/get_selector', async (req, res) => {
     try{
        const result = await GetSelector(req);
@@ -81,6 +123,13 @@ router.get('/get_selector', async (req, res) => {
     }
 })
 
+/**
+ * @param req includes following members
+ * types - comma separated list of parameter types, ex. uint,address,string
+ * values - comma separated list of parameter values, ex. 100,0x7B3AF414448ba906f02a1CA307C56c4ADFF27ce7,hello world
+ * 
+ * @returns hash of these parameters to be used in relay function
+ */
 router.get('/get_encoded_params', async (req, res) => {
     try{
         const result = await GetEncodedParams(req);
@@ -91,6 +140,13 @@ router.get('/get_encoded_params', async (req, res) => {
     }
 })
 
+/**
+ * @param req includes following members
+ * network - 'goerli' or 'mainnet' -> only goerli currently supported
+ * wallet - wallet address of user
+ * 
+ * @returns nonce of wallet according to relayer contract - needed for relay function
+ */
 router.get('/get_relay_nonce', async (req, res) => {
     try{
         const result = await GetRelayNonce(req);
@@ -101,6 +157,12 @@ router.get('/get_relay_nonce', async (req, res) => {
     }
 })
 
+/**
+ * @param req includes following members
+ * wallet - wallet address of user whose balance is being checked
+ * 
+ * @returns array of details about which NFTs are held by this wallet on this network
+ */
 router.get('/owned_nfts', async(req, res) => {
     try{
         const result = await OwnedNFTs(req);
@@ -111,6 +173,12 @@ router.get('/owned_nfts', async(req, res) => {
     }
 });
 
+/**
+ * @param req includes the following members
+ * contract - address of NFT smart contract
+ * 
+ * @returns all wallets who own at least one NFT from given contract
+ */
 router.get('/contract_owners', async (req, res) => {
     try{
         const result = await ContractOwners(req);
@@ -121,6 +189,17 @@ router.get('/contract_owners', async (req, res) => {
     }
 })
 
+/**
+ * @param req includes the following members
+ * wallet - address of user
+ * contract - address of smart contract
+ * 
+ * @returns merkle proof which cryptographically proves 
+ * the given wallet address is on the whitelist for the 
+ * given smart contract address.
+ * 
+ * If wallet is not on whitelist, throws an error.
+ */
 router.get('/merkle_proof', async (req, res) => {
     try{
         const result = await MerkleProof(req);
@@ -131,6 +210,13 @@ router.get('/merkle_proof', async (req, res) => {
     }
 })
 
+/**
+ * @param req includes the following members
+ * contract - address of smart contract in question
+ * 
+ * @returns merkle root for whitelist associated with
+ * contract. Useful to add to smart contract.
+ */
 router.get('/merkle_root', async (req, res) => {
     try{
         const result = await MerkleRoot(req);
@@ -141,6 +227,23 @@ router.get('/merkle_root', async (req, res) => {
     }
 })
 
+/**
+ * @param req includes the following members
+ * name - name of NFT
+ * symbol
+ * maxSupply
+ * price
+ * whitelist_price
+ * wallet - wallet address on whose behalf the contract is being deployed
+ * network - network to deploy contract on - currently only 'goerli' supported
+ * 
+ * signature - signature of wallet
+ * message - message signed by wallet
+ * 
+ * ^ used to authenticate that the request came from the wallet provided
+ * 
+ * @returns address of resulting smart contract
+ */
 router.get('/deploy_nft', async (req, res) => {
     const auth = await SignatureAuth(req);
     !auth ? res.status(401).send('Access Denied') : console.log('authorized');
@@ -153,6 +256,22 @@ router.get('/deploy_nft', async (req, res) => {
     }
 })
 
+/**
+ * @description updates whitelist associated with given smart contract address in 
+ * database and on the smart contract itself - at not cost to the caller.
+ * 
+ * @param req includes following members
+ * contract - address of smart contract
+ * wallets - comma separated list of addresses to add to whitelist
+ *           ex. 0x7B3AF414448ba906f02a1CA307C56c4ADFF27ce7,0x1Dd7134A77f5e3E2E63162bBdcFD494140908270,0x007FB487100f74Bf425B7AdE9Ca0Ae1916f54f11
+ * network - currently only 'goerli' is supported
+ * wallet - address of sender of request - must be owner of the smart contract
+ * 
+ * signature - signature of wallet
+ * message - message signed by wallet
+ * 
+ * ^ used to authenticate that the request came from the wallet provided
+ */
 router.get('/append_whitelist', async (req, res) => {
     const auth = await SignatureAuth(req);
     !auth ? res.status(401).send('Access Denied') : console.log('authorized');
@@ -167,6 +286,14 @@ router.get('/append_whitelist', async (req, res) => {
     }
 })
 
+/**
+ * @param req includes following members
+ * wallet - wallet address being queried about
+ * 
+ * @returns array of smart contract addresses owned by given wallet
+ * 
+ * @note only returns data about contracts deployed through this API
+ */
 router.get('/get_owned_contracts', async (req, res) => {
     try {
         const result = await GetOwnedContracts(req);
@@ -178,6 +305,15 @@ router.get('/get_owned_contracts', async (req, res) => {
     }
 })
 
+/**
+ * @param req includes following members
+ * wallet - address of user being authenticated
+ * message - message being signed by wallet
+ * signature - hash resulting from wallet signature of message
+ * 
+ * @returns true if signature and message recover to given wallet address
+ * false otherwise
+ */
 router.get('/signature_auth', async (req, res) => {
     try {
         const auth = await SignatureAuth(req);
