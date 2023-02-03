ae556b253bed5e5668cd632056cf055af01dcdfc
Adrian Lawson <adrianglawson@gmail.com>
2023-02-03 10:30:52 -0800

    Update GetProvider.js

--- functions/GetProvider.js
+++ functions/GetProvider.js
@@ -1,36 +1,14 @@
 import { ethers } from "ethers"
 
 export const GetProvider = (network) => {
-    let provider
-
-    switch(network) {
-        case 'goerli': 
-            provider = new ethers.providers.JsonRpcProvider(process.env.RPC_GOERLI)
-            break
-
-        case 'mainnet':
-            provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
-            break
-        
-        case 'polygon':
-            provider = new ethers.providers.JsonRpcProvider(process.env.RPC_POLYGON)
-            break
-        
-        case 'arbitrum':
-            provider = new ethers.providers.JsonRpcProvider(process.env.RPC_ARBITRUM)
-            break
-
-        case 'optimism':
-            provider = new ethers.providers.JsonRpcProvider(process.env.RPC_OPTIMISM)
-            break
-        
-        case 'avax':
-            provider = new ethers.providers.JsonRpcProvider(process.env.RPC_AVAX)
-            break
-
-        default:
-            throw 'network is invalid or not supported'
+    const providers = {
+        'goerli': process.env.RPC_GOERLI,
+        'mainnet': process.env.RPC_URL,
+        'polygon': process.env.RPC_POLYGON,
+        'arbitrum': process.env.RPC_ARBITRUM,
+        'optimism': process.env.RPC_OPTIMISM,
+        'avax': process.env.RPC_AVAX
     }
 
-    return provider
+    return new ethers.providers.JsonRpcProvider(providers[network])
 }
